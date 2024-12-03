7563757a6336dfd2c5b2dfefc9a28315fc7f0e6d
EduENTI <eduardo.pordomingo@enti.cat>
2024-12-03 14:30:45 +0100

    Changes

--- client.sh
+++ client.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
 
+echo "1. SEND HEADER"
+
+echo "LSTP_1" | nc localhost 7777
+
+echo "2. LISTEN OK_HEADER"
+
+DATA=`nc -l 7777`
+
+echo "6. CHECK OK_HEADER"
+
+if [ "$DATA" != "OK_HEADER"  ]; then
+
+	echo "ERROR 2: Header not sent correctly. $DATA"
+
+	exit 1
+fi
+
+#text2wave client/lechuga1.lechu -o client/lechuga1.wav
+
+#ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
+
+echo "7. SEND FILE_NAME"
+
+echo "FILE_NAME lechuga1.ogg" | nc localhost 7777
+
+echo "8. LISTEN PREFIX_OK"
--- server.sh
+++ server.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
+echo "LSTP server (Lechuga Speaker Transfer Protocol)"
 
+echo "0. LISTEN"
+
+DATA=`nc -l 7777`
+
+echo "3. CHECK HEADER"
+
+if [ "$DATA" != "LSTP_1"  ]; then
+
+	echo "ERROR 1: Header malformed. $DATA"
+
+	echo "KO_HEADER" | nc localhost 7777
+
+	exit 1
+fi
+
+echo "4. SEND OK_HEADER"
+
+echo "OK_HEADER" | nc localhost 7777
+
+echo "5. LISTEN FILE_NAME"
+
+DATA=`nc -l 7777 | cut -d " " -f 1`
+
+echo "9. CHECK FILE_NAME"
+
+if [ "$DATA" != "FILE_NAME"  ]; then
+	
+	echo "ERROR 3: Prefix unkown. $DATA"
+
+	exit 1
+
+fi
