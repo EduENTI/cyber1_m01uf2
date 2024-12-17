409439a80701a0b977b316e4d423068665b5c53e
EduENTI <eduardo.pordomingo@enti.cat>
2024-12-17 12:28:17 +0100

    Continuation of protocol development

--- client.sh
+++ client.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+PORT=7777
+
+IP_SERVER="localhost"
+
+FILE="saludo.ogg"
+
+IP_CLIENT=`ip a | grep "inet " | grep brd | cut -d " " -f 6 | cut -d "/" -f 1`
+
 echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
 
 echo "1. SEND HEADER"
 
-echo "LSTP_1" | nc localhost 7777
+echo "LSTP_1.1 $IP_CLIENT" | nc $IP_SERVER $PORT
 
 echo "2. LISTEN OK_HEADER"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
 echo "6. CHECK OK_HEADER"
 
 if [ "$DATA" != "OK_HEADER"  ]; then
 
-	echo "ERROR 2: Header not sent correctly. $DATA"
+	echo "ERROR 1: Header not sent correctly. $DATA"
 
 	exit 1
 fi
@@ -25,6 +33,24 @@ fi
 
 echo "7. SEND FILE_NAME"
 
-echo "FILE_NAME lechuga1.ogg" | nc localhost 7777
+echo "FILE_NAME saludo.ogg" | nc $IP_SERVER $PORT
 
 echo "8. LISTEN PREFIX_OK"
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_NAME" ]; then
+	
+	echo "ERROR 2: Filename not set correctly. $DATA"
+
+	exit 2
+
+fi
+
+echo "12. SEND FILE_DATA"
+
+cat "$FILE" | nc $IP_SERVER $PORT
+
+echo "13. LISTEN OK_FILE_DATA"
+
+DATA=`nc -l $PORT`
--- server.sh
+++ server.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+PORT=7777
+
 echo "LSTP server (Lechuga Speaker Transfer Protocol)"
 
 echo "0. LISTEN"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
+
+PROTOCOL_PREFIX=`echo "$DATA" | cut -d " " -f 1`
 
 echo "3. CHECK HEADER"
 
-if [ "$DATA" != "LSTP_1"  ]; then
+if [ "$PROTOCOL_PREFIX" != "LSTP_1.1"  ]; then
 
 	echo "ERROR 1: Header malformed. $DATA"
 
-	echo "KO_HEADER" | nc localhost 7777
+	echo "KO_HEADER" | nc $IP_CLIENT $PORT
 
 	exit 1
 fi
 
+IP_CLIENT=`echo "$DATA" | cut -d " " -f 2`
+
 echo "4. SEND OK_HEADER"
 
-echo "OK_HEADER" | nc localhost 7777
+echo "OK_HEADER" | nc $IP_CLIENT $PORT
 
 echo "5. LISTEN FILE_NAME"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
 FILE_NAME_PREFIX=`echo "$DATA" | cut -d " " -f 1`
 
@@ -31,8 +37,35 @@ echo "9. CHECK FILE_NAME"
 
 if [ "$FILE_NAME_PREFIX" != "FILE_NAME"  ]; then
 	
-	echo "ERROR 3: Prefix unkown. $DATA"
+	echo "ERROR 2: Prefix unkown. $DATA"
 
-	exit 1
+	echo "KO_FILE_NAME"
+	
+	exit 2
 
 fi
+
+FILE_NAME=`echo "$DATA" | cut -d " " -f 2`
+
+echo "10. SEND OK_FILE_NAME"
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "11. LISTEN FILE_DATA"
+
+nc -l $PORT > server/$FILE_NAME
+
+echo "14. SEND OK_FILE_DATA"
+
+FILE_SIZE=`ls -l server/$FILE_NAME | cut -d " " -f 5`
+
+if [ $FILE_SIZE == 0 ]; then
+
+	echo "ERROR 3: No file data. File size: $FILE_SIZE B."
+
+	echo "KO_FILE_DATA"
+
+	exit 3
+
+fi
+ echo "OK_FILE_DATA" | nc $IP_CLIENT $PORT
